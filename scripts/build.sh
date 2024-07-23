e7c0664f257db2628cf155dcdc7f0e64f88d4abb
Vaibhav Singh <vb777222@dal.ca>
2024-07-23 16:15:47 -0300

    chore: minor

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,12 +2,12 @@
 
 echo "Build API..."
 cd api 
-docker buildx build  -t docker.io/vsramchaik/gojo-api --push . 
+docker buildx build --platform linux/amd64,linux/arm64 -t docker.io/vsramchaik/gojo-api --push . 
 cd ..
 echo "Pushed to dockerhub"
 
 echo "Build Web..."
 cd web
-docker buildx build  -t docker.io/vsramchaik/gojo-web --push . 
+docker buildx build --platform linux/amd64,linux/arm64 -t docker.io/vsramchaik/gojo-web --push . 
 cd ..
 echo "Pushed to dockerhub"
